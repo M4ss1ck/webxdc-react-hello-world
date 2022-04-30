6a0fef50c5e2e2e9a15fb5beeab59f310f789abf
M4ss1ck <4ndyraul@gmail.com>
2022-04-30 17:22:47 -0400

    add footer and css import

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,20 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+import "./input.css";
+
 const App = () => {
+  const year = new Date().getFullYear();
   return (
     <div className="flex flex-col items-center justify-center h-[100vh]">
-      <h1 className="text-xl font-bold text-red-600">Hello World</h1>
+      <h1 className="mt-8 text-xl font-bold text-red-600">Hello World</h1>
       <p>Made using React + Tailwind CSS</p>
+      <p className="mt-auto mb-4">&copy;{year} Massick</p>
     </div>
   );
 };
 
-const domContainer = document.querySelector("#react-code");
-ReactDOM.render(React.createElement(App), domContainer);
+const domContainer = document.getElementById("react-code");
+//ReactDOM.render(React.createElement(App), domContainer);
+const root = createRoot(domContainer); // createRoot(container!) if you use TypeScript
+root.render(<App />);
